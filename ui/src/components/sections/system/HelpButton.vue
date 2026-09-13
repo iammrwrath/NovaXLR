1213728cac044fb6ccfaa6edb93d6531cea2d7bc
iammrwrath <294888957+iammrwrath@users.noreply.github.com>
2026-09-13 09:30:48 -0400

    fix: redirect help button to GitHub issue creation instead of Discord

--- ui/src/components/sections/system/HelpButton.vue
+++ ui/src/components/sections/system/HelpButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a href="https://discord.gg/BRBjkkbvmZ" target="_blank">
+    <a href="https://github.com/iammrwrath/NovaXLR/issues/new" target="_blank" @click.prevent="openHelp">
       <BigButton id="help" :title="$t('message.system.help')">
         <font-awesome-icon icon="fa-solid fa-circle-question"/>
       </BigButton>
@@ -13,7 +13,17 @@ import BigButton from "@/components/buttons/BigButton.vue";
 
 export default {
   name: "HelpButton",
-  components: {BigButton}
+  components: {BigButton},
+  methods: {
+    openHelp() {
+      const url = "https://github.com/iammrwrath/NovaXLR/issues/new";
+      if (window.__TAURI__ && window.__TAURI__.opener) {
+        window.__TAURI__.opener.openUrl(url);
+      } else {
+        window.open(url, "_blank");
+      }
+    }
+  }
 }
 </script>
 
